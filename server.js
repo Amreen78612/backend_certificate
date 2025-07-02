ffe5bacba8aa77776a10a2056089507af248cdbc
Amreen78612 <amreensm6264@gmail.com>
2025-07-03 02:20:41 +0530

    api

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const cors = require("cors");
 const PDFDocument = require("pdfkit");
 const fs = require("fs");
 const path = require("path");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/generate-certificate", (req, res) => {
+
+app.post(`/generate-certificate`, (req, res) => {
   const { name } = req.body;
 
   if (!name) {
@@ -107,7 +109,7 @@ app.post("/generate-certificate", (req, res) => {
   doc.end();
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });

0aa8b5bf38adeb436fad54224466865773bcb16b
Amreen78612 <amreensm6264@gmail.com>
2025-07-03 18:14:23 +0530

    deploye

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*', // or specific frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -114,7 +118,7 @@ app.post(`/generate-certificate`, (req, res) => {
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
